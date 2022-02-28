9df867e0b85c354177353964e00a3c04006aa5e0
SirSydom <com@sirsydom.de>
2022-02-28 15:07:37 +0100

    added OpenKNX static class

---
+++ src/OpenKNX.cpp
@@ -0,0 +1,46 @@
+//#include <knx.h>
+#include "src/extlibs/knx/knx.h"
+#include "OpenKNX.h"
+
+VersionCheckResult OpenKNX::versionCheck(uint16_t manufacturerId, uint8_t *hardwareType)
+{
+    VersionCheckResult check = FlashAllInvalid;
+    if (manufacturerId == 0x00FA)
+    {
+        // hardwareType has the format 0x00 00 Ap nn vv 00
+        if (memcmp(knx.bau().deviceObject().hardwareType(), hardwareType, 4) == 0)
+        {
+            check = FlashTablesInvalid;
+            if (knx.bau().deviceObject().hardwareType()[4] == hardwareType[4])
+            {
+                check = FlashValid;
+            }
+            else
+            {
+                println("ApplicationVersion changed, ETS has to reprogram the application!");
+            }
+        }
+    }
+    else
+    {
+        println("This firmware supports only applicationId 0x00FA");
+    }
+    return check;
+}
+
+void OpenKNX::knxRead(uint8_t openKnxId, uint8_t applicationNumber, uint8_t applicationVersion, uint8_t firmwareRevision)
+{
+    uint8_t hardwareType[LEN_HARDWARE_TYPE] = {0x00, 0x00, openKnxId, applicationNumber, applicationVersion, 0x00};
+
+    // first setup flash version check
+    knx.bau().addVersionCheckCallback(versionCheck);
+    // set correct hardware type for flash compatibility check
+    knx.bau().deviceObject().hardwareType(hardwareType);
+    // read flash data
+    knx.readMemory();
+    // set hardware type again, in case an other hardware type was deserialized from flash
+    knx.bau().deviceObject().hardwareType(hardwareType);
+    // set firmware version als user info (PID_VERSION)
+    // 5 bit major, 5 bit minor, 6 bit revision
+    knx.bau().deviceObject().version(((applicationVersion & 0xF0) << 7) | ((applicationVersion & 0xF) << 6) | (firmwareRevision & 0x3F));
+}
---
+++ src/OpenKNX.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Arduino.h>
+
+
+class OpenKNX
+{
+  public:
+    // this function is a replacement for knx.read() and implements version checks for OpenKNX
+    // it ensures, that only correct applicatins can be loaded from flash
+    static void knxRead(uint8_t openKnxId, uint8_t applicationNumber, uint8_t applicationVersion, uint8_t firmwareRevision);
+
+    // this function is called during load of knx data from flash and cecks for version compatibility
+    static VersionCheckResult versionCheck(uint16_t manufacturerId, uint8_t *hardwareType);
+};
