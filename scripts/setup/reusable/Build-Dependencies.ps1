43df8d6876ca3329fb16c53252317c0a17966b5e
Waldemar Porscha <wp@porscha.eu>
2022-11-30 09:44:44 +0100

    - Restore scripts - save work

--- scripts/setup/reusable/Build-Dependencies.ps1
+++ scripts/setup/reusable/Build-Dependencies.ps1
@@ -1,5 +1,19 @@
-$subprojects = Get-ChildItem -Directory lib
 Write-Output "------- Build with -------" > dependencies.txt
+$subprojects = Get-ChildItem -Directory lib
+# $project = $(Split-Path $(Get-Location) -Leaf)
+$projects = @()
+# $projects += "." #  $project
 foreach ($subproject in $subprojects) {
-    git --git-dir lib/$subproject/.git log -1 --pretty=format:"%h $subproject" >> dependencies.txt
+    $projects += "lib/" + $subproject.Name
 }
+
+# $branch = git branch --show-current
+# $subproject = Split-Path $(Get-Location) -Leaf 
+# git log -1 --pretty=format:"%h $branch $subproject" >> dependencies.txt
+foreach ($subproject in $projects) {
+    $branch = git --git-dir $subproject/.git branch --show-current
+    git --git-dir $subproject/.git log -1 --pretty=format:"%h $branch $subproject" >> dependencies.txt
+}
+
+
+
