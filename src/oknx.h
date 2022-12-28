5617a37d756a21c6007474a004498f21829a4205
Waldemar Porscha <wp@porscha.eu>
2022-12-28 11:40:21 +0100

    Added readMemory to facade

--- src/oknx.h
+++ src/oknx.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "knx.h"
+#include "OpenKNX.h"
 #include "FlashUserData.h"
 
 class OpenKNXfacade
@@ -8,8 +9,7 @@ class OpenKNXfacade
 private:
     FlashUserData* _flashUserDataPtr;
 public:
-    OpenKNXfacade() : _flashUserDataPtr(new FlashUserData())
-    {};
+    OpenKNXfacade() : _flashUserDataPtr(new FlashUserData()) {};
     ~OpenKNXfacade() {};
 
     FlashUserData* flashUserData() 
@@ -21,6 +21,12 @@ public:
         _flashUserDataPtr->loop();
         knx.loop();
     }
+
+    void readMemory(uint8_t openKnxId, uint8_t applicationNumber, uint8_t applicationVersion, uint8_t firmwareRevision, const char* OrderNo = nullptr)
+    {
+        OpenKNX::knxRead(openKnxId, applicationNumber, applicationVersion, firmwareRevision, OrderNo);
+    }
+
 };
 
 extern OpenKNXfacade openknx;
