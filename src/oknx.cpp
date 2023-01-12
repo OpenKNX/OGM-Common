c69f8f99bc7aa9d0f5b88ed91b8e556421dbdcd8
Waldemar Porscha <wp@porscha.eu>
2023-01-13 00:40:11 +0100

    Move implementation from .h to .cpp

--- src/oknx.cpp
+++ src/oknx.cpp
@@ -1,3 +1,19 @@
 #include "oknx.h"
 
 OpenKNXfacade openknx;
+
+FlashUserData* OpenKNXfacade::flashUserData() 
+{
+    return _flashUserDataPtr; 
+}
+
+void OpenKNXfacade::loop() {
+    _flashUserDataPtr->loop();
+    knx.loop();
+}
+
+void OpenKNXfacade::readMemory(uint8_t openKnxId, uint8_t applicationNumber, uint8_t applicationVersion, uint8_t firmwareRevision, const char* OrderNo /*= nullptr*/)
+{
+    OpenKNX::knxRead(openKnxId, applicationNumber, applicationVersion, firmwareRevision, OrderNo);
+}
+
--- src/oknx.h
+++ src/oknx.h
@@ -8,25 +8,14 @@ class OpenKNXfacade
 {
 private:
     FlashUserData* _flashUserDataPtr;
+    
 public:
     OpenKNXfacade() : _flashUserDataPtr(new FlashUserData()) {};
     ~OpenKNXfacade() {};
 
-    FlashUserData* flashUserData() 
-    {
-        return _flashUserDataPtr; 
-    }
-
-    void loop() {
-        _flashUserDataPtr->loop();
-        knx.loop();
-    }
-
-    void readMemory(uint8_t openKnxId, uint8_t applicationNumber, uint8_t applicationVersion, uint8_t firmwareRevision, const char* OrderNo = nullptr)
-    {
-        OpenKNX::knxRead(openKnxId, applicationNumber, applicationVersion, firmwareRevision, OrderNo);
-    }
-
+    FlashUserData* flashUserData(); 
+    void loop();
+    void readMemory(uint8_t openKnxId, uint8_t applicationNumber, uint8_t applicationVersion, uint8_t firmwareRevision, const char* OrderNo = nullptr);
 };
 
 extern OpenKNXfacade openknx;
