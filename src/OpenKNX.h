94d76d2968e5cfc7ad50cffada5f893c401716b1
Mike <45664417+thewhobox@users.noreply.github.com>
2023-04-24 10:44:32 +0200

    added error for not defined SMALL_GROUPOBJECT

--- src/OpenKNX.h
+++ src/OpenKNX.h
@@ -1,4 +1,9 @@
 #pragma once
+
+#ifndef SMALL_GROUPOBJECT
+    #error OGM-Common needs build-flag "-D SMALL_GROUPOBJECT"
+#endif
+
 #include "Helper.h"
 #include "OpenKNX/Channel.h"
 #include "OpenKNX/Common.h"
