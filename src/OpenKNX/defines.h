a93c6ff34856390a1b03c15186d2f77f53308fc2
Marco Scholl <develop@marco-scholl.de>
2023-06-02 01:36:04 +0200

    fix: includes for samd

--- src/OpenKNX/defines.h
+++ src/OpenKNX/defines.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "hardware.h"
-#include "knxprod.h"
-#include "versions.h"
+#include <hardware.h>
+#include <knxprod.h>
+#include <versions.h>
 
 // see README for available defines
 
