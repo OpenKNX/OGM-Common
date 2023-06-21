c768c21f59e64969ab0c5c94d249e1c595db465a
Ing-Dom <dom@ing-dom.de>
2023-06-21 14:55:58 +0200

    moved board and mask definition to the project.ini to enable more easy changing. added default values to the example.ini changed rp2040 dtection in show_flash_partitioning.py to enable other boards

--- show_flash_partitioning.py
+++ show_flash_partitioning.py
@@ -116,7 +116,7 @@ def show_flash_partitioning(source, target, env):
         if m is not None:
             size += int(m.group(1))
         
-        if projenv['BOARD'] == 'pico':
+        if projenv['PIOPLATFORM'] == 'raspberrypi':
             size -= 268435456 # subtract start of flash address pointer
 
         return size
@@ -149,7 +149,7 @@ def show_flash_partitioning(source, target, env):
     flash_end = 0
 
     firmware_end = firmware_size(env)
-    if projenv['BOARD'] == 'pico':
+    if projenv['PIOPLATFORM'] == 'raspberrypi':
         eeprom_start = env["PICO_EEPROM_START"] - 268435456
         flash_end = eeprom_start + 4096
         system_end = eeprom_start
