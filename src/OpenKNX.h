c4ac9ec398bd508b487cecd947fa7c511c60e4a7
Marco Scholl <develop@marco-scholl.de>
2023-08-07 21:56:10 +0200

    feat: added architecture compatibility check

--- src/OpenKNX.h
+++ src/OpenKNX.h
@@ -4,6 +4,10 @@
 #error OGM-Common needs build-flag "-D SMALL_GROUPOBJECT"
 #endif
 
+#if !defined(ARDUINO_ARCH_SAMD) && !defined(ARDUINO_ARCH_RP2040) && !defined(ARDUINO_ARCH_ESP32)
+#error Your archetecture is not supported by OpenKNX
+#endif
+
 #include "OpenKNX/Channel.h"
 #include "OpenKNX/Common.h"
 #include "OpenKNX/Facade.h"
