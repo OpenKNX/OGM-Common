e04c198f56bc01205beb6aab8cd3464645a70428
Cornelius Köpp <cornelius.koepp@fhdw.de>
2023-08-11 16:35:36 +0200

    Runtime Statistics: Begin Extraction of DurationStatistic (WIP)

--- src/OpenKNX/RuntimeStat.cpp
+++ src/OpenKNX/RuntimeStat.cpp
@@ -10,12 +10,12 @@
 namespace OpenKNX
 {
 
-    uint32_t RuntimeStat::_timeRangeMax[OPENKNX_RUNTIME_STAT_BUCKETN] = {
+    uint32_t DurationStatistic::_timeRangeMax[OPENKNX_RUNTIME_STAT_BUCKETN] = {
         OPENKNX_RUNTIME_STAT_BUCKETS,
         0xffffffff, // max value, so we do not need a special case
     };
 
-    uint8_t RuntimeStat::calcBucketIndex(const uint32_t value_us)
+    uint8_t DurationStatistic::calcBucketIndex(const uint32_t value_us)
     {
         uint8_t i = 0;
         while (_timeRangeMax[i] < value_us)
@@ -25,27 +25,27 @@ namespace OpenKNX
         return i;
     }
 
-    uint32_t RuntimeStat::calcBucketMax(const uint8_t bucketIndex, const uint32_t overallMax)
+    uint32_t DurationStatistic::calcBucketMax(const uint8_t bucketIndex)
     {
         const uint32_t bucketMax = _timeRangeMax[bucketIndex];
-        return MIN(bucketMax, overallMax);
+        return MIN(bucketMax, durationMax_us);
     }
 
-    uint32_t RuntimeStat::calcBucketMin(const uint8_t bucketIndex, const uint32_t overallMin)
+    uint32_t DurationStatistic::calcBucketMin(const uint8_t bucketIndex)
     {
         const uint32_t bucketMin = bucketIndex==0 ? 0 : _timeRangeMax[bucketIndex-1];
-        return MAX(bucketMin, overallMin);
+        return MAX(bucketMin, durationMin_us);
     }
 
-    uint32_t RuntimeStat::estimateMedian(DurationStatistic &stat)
+    uint32_t DurationStatistic::estimateMedian()
     {
         // TODO special handling of edge-cases!
 
         if (_count == 0)
-            return stat.durationMin_us;
+            return durationMin_us;
 
         if (_count <= 2)
-            return (stat.durationMin_us + stat.durationMax_us) / 2;
+            return (durationMin_us + durationMax_us) / 2;
 
         uint8_t medianIndex = 0;
         const uint32_t medianCount = _count / 2;
@@ -53,7 +53,7 @@ namespace OpenKNX
         uint32_t cumulatedCountUpper = 0;
         for (size_t i = 0; i < OPENKNX_RUNTIME_STAT_BUCKETN; i++)
         {
-            cumulatedCountUpper += stat.durationBucket[i];
+            cumulatedCountUpper += durationBucket[i];
             if (cumulatedCountUpper >= medianCount)
             {
                 // found the bucket containing the value
@@ -64,16 +64,26 @@ namespace OpenKNX
         }
 
         // median must be in the closed interval defined by the intersection of selected bucket and [min;max]
-        const uint32_t medianMin = calcBucketMin(medianIndex, stat.durationMin_us);
-        const uint32_t medianMax = calcBucketMax(medianIndex, stat.durationMax_us);
+        const uint32_t medianMin = calcBucketMin(medianIndex);
+        const uint32_t medianMax = calcBucketMax(medianIndex);
 
         // "The ``best'' estimate for the mean [and median] is obtained by assuming the data is uniformly spread within each interval"
         // [http://www.cs.uni.edu/~campbell/stat/histrev2.html accessed 2023-08-06]
         // Using information of min- and maximum value can reduce the interval and thereby improve the result.
-        double factor = 1.0 * (medianCount - cumulatedCountLower) / stat.durationBucket[medianIndex];
+        double factor = 1.0 * (medianCount - cumulatedCountLower) / durationBucket[medianIndex];
         return  medianMin + (medianMax - medianMin) * factor;
 
         // TODO check usage of one side open interval?
+    }
+
+    void DurationStatistic::measure(const uint32_t duration_us)
+    {
+        durationBucket[calcBucketIndex(duration_us)]++;
+        durationMax_us = MAX(durationMax_us, duration_us);
+        durationMin_us = MIN(durationMin_us, duration_us);
+        sum_us += duration_us;
+
+        _count++;
     }
 
     void RuntimeStat::measureTimeBegin()
@@ -83,13 +93,7 @@ namespace OpenKNX
         // measure waiting-time between two loops
         if (_end_us > 0)
         {
-            const uint32_t waiting_duration_us = _begin_us - _end_us;
-
-            _wait.durationBucket[calcBucketIndex(waiting_duration_us)]++;
-
-            _wait.durationMax_us = MAX(_wait.durationMax_us, waiting_duration_us);
-            _wait.durationMin_us = MIN(_wait.durationMin_us, waiting_duration_us);
-            _wait.sum_us += waiting_duration_us;
+            _wait.measure(_begin_us - _end_us);
         }
     }
 
@@ -98,21 +102,13 @@ namespace OpenKNX
         // store end only once at the beginning, as getting the time twice might increase error
         _end_us = micros();
 
-        const uint32_t duration_us = _end_us - _begin_us;
-        _run.durationBucket[calcBucketIndex(duration_us)]++;
-
-        _run.durationMax_us = MAX(_run.durationMax_us, duration_us);
-        _run.durationMin_us = MIN(_run.durationMin_us, duration_us);
-        _run.sum_us += duration_us;
-
-        // TODO check removal (is included in _countRunDuration)
-        _count++;
+        _run.measure(_end_us - _begin_us);
     }
 
     void RuntimeStat::showStat(std::string label)
     {
-        const uint32_t runCount = _count;
-        const uint32_t waitCount = _count - 1;
+        const uint32_t runCount = _run._count;
+        const uint32_t waitCount = _wait._count;
 
         openknx.logger.logWithPrefixAndValues(label, "#    count     %10d %10d",
             runCount, waitCount
@@ -128,8 +124,8 @@ namespace OpenKNX
             _wait.sum_us / waitCount
         );
         openknx.logger.logWithPrefixAndValues(label, "us    ~med     %10d %10d",
-            estimateMedian(_run),
-            estimateMedian(_wait)
+            _run.estimateMedian(),
+            _wait.estimateMedian()
         );
         openknx.logger.logWithPrefixAndValues(label, "us     max     %10d %10d",
             _run.durationMax_us, _wait.durationMax_us
@@ -137,7 +133,7 @@ namespace OpenKNX
         for (size_t i = 0; i < OPENKNX_RUNTIME_STAT_BUCKETN; i++)
         {
             openknx.logger.logWithPrefixAndValues(label, "#<= %6d us  %10d %10d",
-                _timeRangeMax[i], _run.durationBucket[i], _wait.durationBucket[i]
+                DurationStatistic::_timeRangeMax[i], _run.durationBucket[i], _wait.durationBucket[i]
             );
         }
         
--- src/OpenKNX/RuntimeStat.h
+++ src/OpenKNX/RuntimeStat.h
@@ -18,36 +18,38 @@
     #define OPENKNX_RUNTIME_STAT_BUCKETN 16
 #endif
 
-struct DurationStatistic
-{
-  uint64_t sum_us = 0;
-  uint32_t durationMin_us = 0xffffffffu;
-  uint32_t durationMax_us = 0;
-  uint32_t durationBucket[OPENKNX_RUNTIME_STAT_BUCKETN];
-};
-
-
-
 namespace OpenKNX
 {
+
+    class DurationStatistic
+    {
+      private:
+      public:
+        static uint32_t _timeRangeMax[OPENKNX_RUNTIME_STAT_BUCKETN];
+
+        uint32_t _count = 0;
+        uint64_t sum_us = 0;
+        uint32_t durationMin_us = 0xffffffffu;
+        uint32_t durationMax_us = 0;
+        uint32_t durationBucket[OPENKNX_RUNTIME_STAT_BUCKETN];
+
+        static uint8_t calcBucketIndex(const uint32_t value_us);
+
+        void measure(const uint32_t duration_us);
+        uint32_t calcBucketMax(const uint8_t bucketIndex);
+        uint32_t calcBucketMin(const uint8_t bucketIndex);
+        uint32_t estimateMedian();
+
+    };
+
     class RuntimeStat
     {
       private:
-        static uint32_t _timeRangeMax[OPENKNX_RUNTIME_STAT_BUCKETN];
-
         uint32_t _begin_us = 0;
         uint32_t _end_us = 0;
 
-        uint32_t _count = 0;
-
         DurationStatistic _run;
         DurationStatistic _wait;
-        
-        static uint8_t calcBucketIndex(const uint32_t value_us);
-
-        uint32_t calcBucketMax(const uint8_t bucketIndex, const uint32_t overallMax);
-        uint32_t calcBucketMin(const uint8_t bucketIndex, const uint32_t overallMin);
-        uint32_t estimateMedian(DurationStatistic &stat);
 
       protected:
 
