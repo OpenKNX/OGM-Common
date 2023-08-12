05c58c60fa8ffc4406be2c6e058be3110b2430e3
Cornelius Köpp <cornelius.koepp@fhdw.de>
2023-08-12 17:13:33 +0200

    Version Output on Console: Firmware First and Add Commons

--- src/OpenKNX/Console.cpp
+++ src/OpenKNX/Console.cpp
@@ -269,8 +269,9 @@ namespace OpenKNX
         openknx.logger.log("======================== Versions ==============================================");
         openknx.logger.color(0);
 
-        openknx.logger.logWithPrefix("KNX", KNX_Version);
-        openknx.logger.logWithPrefix("Firmware", MAIN_Version);
+        openknx.logger.logWithPrefix("This Firmware", MAIN_Version);
+        openknx.logger.logWithPrefix("KNX-Stack", KNX_Version);
+        openknx.logger.logWithPrefix("OpenKNX OGM-Common", MODULE_Common_Version);
         for (uint8_t i = 0; i < openknx.modules.count; i++)
             openknx.logger.logWithPrefix(openknx.modules.list[i]->name().c_str(), openknx.modules.list[i]->version().c_str());
 
