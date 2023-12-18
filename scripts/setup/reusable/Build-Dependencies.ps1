4d295014c7e8fc6b791bebbcca32e773bd2e123d
Erkan Çolak <erkanc@gmx.de>
2023-12-18 18:27:16 +0100

    Initial - Added MacOS and Linux support

--- scripts/setup/reusable/Build-Dependencies.ps1
+++ scripts/setup/reusable/Build-Dependencies.ps1
@@ -1,4 +1,22 @@
-Write-Output "------- Built with -------" > dependencies.txt
+# Initial Write-Output to dependencies.txt
+# This is a OpenKNX dependencies file. You can enter information about your project dependencies here.
+# Each line represents a dependency and should contain the following information:
+# Hash value, branch, folder path, Git URL, and optionally the branch name.
+#
+# Example:
+# a1b2c3d4 master lib/MyProject https://github.com/username/myproject.git#master
+# a2b3c4d5 dev_1 lib/OpenKNX https://github.com/username/OpenKNX.git
+#
+# You can also add comments by starting a line with '#'.
+# Example:
+# # This is a comment.
+#
+# Please note that lines starting with '-------' are ignored.
+$initContent = @"
+------- Built with -------
+"@
+
+Write-Output $initContent > dependencies.txt
 $subprojects = Get-ChildItem -Directory lib
 # $project = $(Split-Path $(Get-Location) -Leaf)
 $projects = @()
