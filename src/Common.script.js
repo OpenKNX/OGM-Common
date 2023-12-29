dd900f5211c46202bafbeabbeaf21b168fedf7e0
Cornelius Köpp <cornelius.koepp@fhdw.de>
2023-12-29 21:28:14 +0100

    Add Util-Function to Delay Script Execution (e.g. Waiting in Device-Communication)

--- src/Common.script.js
+++ src/Common.script.js
@@ -5,3 +5,19 @@ function newline(device, online, progress, context) {
     var replaced = text.value.split("\\n").join("\n");
     text.value = replaced;
 }
+
+// TODO check split in separate files, to include requried functions only
+
+// helper-functions to delay script execution, e.g. waiting in device-communication
+function openKnxWait_ms(millis) {
+    var start = new Date();
+    var count = 0;
+    // busy waiting, as there is no other known possibility in ETS
+    while (new Date() - start < millis) {
+        count++;
+    }
+    return count;
+}
+function openKnxWait_s(seconds) {
+    return openKnxWait_ms(seconds * 1000);
+}
