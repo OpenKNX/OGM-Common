d5594e8436c0a070e30baeccfaafe6d7e701cc5c
Erkan Çolak <erkanc@gmx.de>
2024-01-14 15:09:45 +0100

    Remove new opknxhw legacy samd

--- include/OpenKNXHardware.h
+++ include/OpenKNXHardware.h
@@ -32,7 +32,6 @@
         #define PIN_ETH_INT (17)
         #define PIN_ETH_RES (18)
 
-
     #endif
 
 // REG1-Base
@@ -110,7 +109,7 @@
         #define KNX_UART_TX_PIN 0
     #endif
 
-    #ifdef OKNXHW_REG1_CONTROLLER2040_V1  // V00.90 - V01.89
+    #ifdef OKNXHW_REG1_CONTROLLER2040_V1 // V00.90 - V01.89
         #define PROG_LED_PIN 25
         #define PROG_LED_PIN_ACTIVE_ON HIGH
         #define PROG_BUTTON_PIN 23
@@ -125,16 +124,16 @@
 // https://github.com/OpenKNX/OpenKNX/wiki/REG1-IpController2040
 
     #ifdef OKNXHW_REG1_IPCONTROLLER2040
-        //#define PROG_LED_PIN 2
-        //#define PROG_LED_PIN_ACTIVE_ON HIGH
-        //#define PROG_BUTTON_PIN 7
-        //#define PROG_BUTTON_PIN_INTERRUPT_ON FALLING
-        //#define SAVE_INTERRUPT_PIN 6
-        //#define INFO_LED_PIN 3
-        //#define INFO_LED_PIN_ACTIVE_ON HIGH
-        //#define KNX_UART_RX_PIN -1
-        //#define KNX_UART_TX_PIN -1
-        
+    // #define PROG_LED_PIN 2
+    // #define PROG_LED_PIN_ACTIVE_ON HIGH
+    // #define PROG_BUTTON_PIN 7
+    // #define PROG_BUTTON_PIN_INTERRUPT_ON FALLING
+    // #define SAVE_INTERRUPT_PIN 6
+    // #define INFO_LED_PIN 3
+    // #define INFO_LED_PIN_ACTIVE_ON HIGH
+    // #define KNX_UART_RX_PIN -1
+    // #define KNX_UART_TX_PIN -1
+
         #define ETH_SPI_INTERFACE SPI // SPI or SPI1, depends on the pins
         #define PIN_ETH_MISO (0)
         #define PIN_ETH_MOSI (3)
@@ -162,42 +161,42 @@
 // https://github.com/OpenKNX/OpenKNX-Pi-Pico-REG2
     #ifdef OKNXHW_REG2_PIPICO_W_ETH_V1
         #define OKNXHW_REG2_PIPICO_W_V1
-            #define ETH_SPI_INTERFACE SPI // SPI or SPI1, depends on the pins
-            #define PIN_ETH_MISO  (16) // ETH_MISO  - SPI0 RX UART0 TX I2C0 SDA PWM0_A SIO PIO0 PIO1
-            #define PIN_ETH_SS    (17) // ETH_CS    - SPI0 CSn UART0 RX I2C0 SCL PWM0_B SIO PIO0 PIO1
-            #define PIN_ETH_SCK   (18) // ETH_SCK   - SPI0 SCK UART0 CTS I2C1 SDA PWM1_A SIO PIO0 PIO1
-            #define PIN_ETH_MOSI  (19) // ETH_MOSI  - SPI0 TX UART0 RTS I2C1 SCL PWM1_B SIO PIO0 PIO1
-            #define PIN_ETH_INT   (26) // ETH_RES   - SPI1 SCK UART1 CTS I2C1 SDA PWM5_A SIO PIO0 PIO1
-            #define PIN_ETH_RES   (27) // ETH_INT   - SPI1 TX UART1 RTS I2C1 SCL PWM5_B SIO PIO0 PIO1
+        #define ETH_SPI_INTERFACE SPI // SPI or SPI1, depends on the pins
+        #define PIN_ETH_MISO (16)     // ETH_MISO  - SPI0 RX UART0 TX I2C0 SDA PWM0_A SIO PIO0 PIO1
+        #define PIN_ETH_SS (17)       // ETH_CS    - SPI0 CSn UART0 RX I2C0 SCL PWM0_B SIO PIO0 PIO1
+        #define PIN_ETH_SCK (18)      // ETH_SCK   - SPI0 SCK UART0 CTS I2C1 SDA PWM1_A SIO PIO0 PIO1
+        #define PIN_ETH_MOSI (19)     // ETH_MOSI  - SPI0 TX UART0 RTS I2C1 SCL PWM1_B SIO PIO0 PIO1
+        #define PIN_ETH_INT (26)      // ETH_RES   - SPI1 SCK UART1 CTS I2C1 SDA PWM5_A SIO PIO0 PIO1
+        #define PIN_ETH_RES (27)      // ETH_INT   - SPI1 TX UART1 RTS I2C1 SCL PWM5_B SIO PIO0 PIO1
     #endif
 
     #ifdef OKNXHW_REG2_PIPICO_ETH_V1
         #define OKNXHW_REG2_PIPICO_V1
-            #define ETH_SPI_INTERFACE SPI // SPI or SPI1, depends on the pins
-            #define PIN_ETH_MISO  (16) // ETH_MISO  - SPI0 RX UART0 TX I2C0 SDA PWM0_A SIO PIO0 PIO1
-            #define PIN_ETH_SS    (17) // ETH_CS    - SPI0 CSn UART0 RX I2C0 SCL PWM0_B SIO PIO0 PIO1
-            #define PIN_ETH_SCK   (18) // ETH_SCK   - SPI0 SCK UART0 CTS I2C1 SDA PWM1_A SIO PIO0 PIO1
-            #define PIN_ETH_MOSI  (19) // ETH_MOSI  - SPI0 TX UART0 RTS I2C1 SCL PWM1_B SIO PIO0 PIO1
-            #define PIN_ETH_INT   (26) // ETH_RES   - SPI1 SCK UART1 CTS I2C1 SDA PWM5_A SIO PIO0 PIO1
-            #define PIN_ETH_RES   (27) // ETH_INT   - SPI1 TX UART1 RTS I2C1 SCL PWM5_B SIO PIO0 PIO1
+        #define ETH_SPI_INTERFACE SPI // SPI or SPI1, depends on the pins
+        #define PIN_ETH_MISO (16)     // ETH_MISO  - SPI0 RX UART0 TX I2C0 SDA PWM0_A SIO PIO0 PIO1
+        #define PIN_ETH_SS (17)       // ETH_CS    - SPI0 CSn UART0 RX I2C0 SCL PWM0_B SIO PIO0 PIO1
+        #define PIN_ETH_SCK (18)      // ETH_SCK   - SPI0 SCK UART0 CTS I2C1 SDA PWM1_A SIO PIO0 PIO1
+        #define PIN_ETH_MOSI (19)     // ETH_MOSI  - SPI0 TX UART0 RTS I2C1 SCL PWM1_B SIO PIO0 PIO1
+        #define PIN_ETH_INT (26)      // ETH_RES   - SPI1 SCK UART1 CTS I2C1 SDA PWM5_A SIO PIO0 PIO1
+        #define PIN_ETH_RES (27)      // ETH_INT   - SPI1 TX UART1 RTS I2C1 SCL PWM5_B SIO PIO0 PIO1
     #endif
 
-    #ifdef  OKNXHW_REG2_PIPICO_W_V1
+    #ifdef OKNXHW_REG2_PIPICO_W_V1
         #define OKNXHW_REG2_PIPICO_V1_BASE
-            #define INFO1_LED_PIN  32  // PiPicoW Onboard LED
-            #define INFO1_LED_PIN_ACTIVE_ON HIGH
-            #define INFO2_LED_PIN 3
-            #define INFO2_LED_PIN_ACTIVE_ON HIGH
-            #define SAVE_INTERRUPT_PIN 21
+        #define INFO1_LED_PIN 32 // PiPicoW Onboard LED
+        #define INFO1_LED_PIN_ACTIVE_ON HIGH
+        #define INFO2_LED_PIN 3
+        #define INFO2_LED_PIN_ACTIVE_ON HIGH
+        #define SAVE_INTERRUPT_PIN 21
     #endif
-      
-    #ifdef  OKNXHW_REG2_PIPICO_V1
+
+    #ifdef OKNXHW_REG2_PIPICO_V1
         #define OKNXHW_REG2_PIPICO_V1_BASE
-            #define INFO1_LED_PIN  25  // PiPico Onboard LED
-            #define INFO1_LED_PIN_ACTIVE_ON HIGH
-            #define INFO2_LED_PIN 3
-            #define INFO2_LED_PIN_ACTIVE_ON HIGH
-            #define SAVE_INTERRUPT_PIN 21
+        #define INFO1_LED_PIN 25 // PiPico Onboard LED
+        #define INFO1_LED_PIN_ACTIVE_ON HIGH
+        #define INFO2_LED_PIN 3
+        #define INFO2_LED_PIN_ACTIVE_ON HIGH
+        #define SAVE_INTERRUPT_PIN 21
     #endif
 
     #ifdef OKNXHW_REG2_PIPICO_V1_BASE
@@ -211,19 +210,19 @@
     #endif
 
 // OpenKNXiao (Base) V1
-    #ifdef  OKNXHW_OPENKNXIAO_KNEOPIX_V1
+    #ifdef OKNXHW_OPENKNXIAO_KNEOPIX_V1
         #define OKNXHW_OPENKNXIAO_RP2040_BASE_V1
-            #define INFO1_LED_PIN 16  // Build-In LED GREEN
-            #define INFO1_LED_PIN_ACTIVE_ON HIGH
-            #define INFO2_LED_PIN 25  // Build-In LED BLUE
-            #define INFO2_LED_PIN_ACTIVE_ON HIGH
+        #define INFO1_LED_PIN 16 // Build-In LED GREEN
+        #define INFO1_LED_PIN_ACTIVE_ON HIGH
+        #define INFO2_LED_PIN 25 // Build-In LED BLUE
+        #define INFO2_LED_PIN_ACTIVE_ON HIGH
     #endif
-    #ifdef  OKNXHW_OPENKNXIAO_MINI_V1
+    #ifdef OKNXHW_OPENKNXIAO_MINI_V1
         #define OKNXHW_OPENKNXIAO_RP2040_BASE_V1
-            #define INFO1_LED_PIN 16  // Build-In LED GREEN
-            #define INFO1_LED_PIN_ACTIVE_ON HIGH
-            #define INFO2_LED_PIN 25  // Build-In LED BLUE
-            #define INFO2_LED_PIN_ACTIVE_ON HIGH
+        #define INFO1_LED_PIN 16 // Build-In LED GREEN
+        #define INFO1_LED_PIN_ACTIVE_ON HIGH
+        #define INFO2_LED_PIN 25 // Build-In LED BLUE
+        #define INFO2_LED_PIN_ACTIVE_ON HIGH
     #endif
 
     #ifdef OKNXHW_OPENKNXIAO_RP2040_BASE_V1
