0b7e4fd49f38dbf60191348619b1914dc80eae21
Ing-Dom <dom@ing-dom.de>
2024-09-10 17:44:39 +0200

    draft

--- include/OpenKNXHardware.h
+++ include/OpenKNXHardware.h
@@ -1,7 +1,7 @@
 // This file defines hardware properties of several reusable OpenKNX hardware
 // It is meant to be included in the project-specific xyzHardware.h
 
-#ifdef ARDUINO_ARCH_RP2040
+
 // REG1-Eth
 // https://github.com/OpenKNX/OpenKNX/wiki/REG1-Eth
 
@@ -79,6 +79,27 @@
 
     #endif
 
+// SEN-REG1-Multi
+// https://github.com/OpenKNX/OpenKNX/wiki/xxxxxx
+    #ifdef OKNXHW_SEN_REG1_MULTI
+        #define HARDWARE_NAME "SEN-REG1-Multi"
+        #define OKNXHW_REG1_CONTROLLER2040_V1
+
+        //#define INFO1_LED_PIN 6
+        //#define INFO1_LED_PIN_ACTIVE_ON HIGH
+
+        //#define INFO2_LED_PIN 24
+        //#define INFO2_LED_PIN_ACTIVE_ON HIGH
+
+        //#define INFO3_LED_PIN 22
+        //#define INFO3_LED_PIN_ACTIVE_ON HIGH
+
+        //#define FUNC1_BUTTON_PIN 7
+
+        #define OKNXHW_REG1_APP_SEN_MULTI
+
+    #endif
+
 // PiPico-BCU-Connector
 // https://github.com/OpenKNX/OpenKNX/wiki/PiPico-BCU-Connector
 
@@ -110,6 +131,9 @@
     #endif
 
     #ifdef OKNXHW_REG1_CONTROLLER2040_V1 // V00.90 - V01.89
+        #ifndef HARDWARE_NAME
+            #define HARDWARE_NAME "OpenKNX-REG1-Controller2040-V1"
+        #endif
         #define PROG_LED_PIN 25
         #define PROG_LED_PIN_ACTIVE_ON HIGH
         #define PROG_BUTTON_PIN 23
@@ -118,6 +142,14 @@
         #define KNX_SERIAL Serial1
         #define KNX_UART_RX_PIN 1
         #define KNX_UART_TX_PIN 0
+
+        #define REG1_APP_PIN1 29
+        #define REG1_APP_PIN2 28
+        #define REG1_APP_PIN3 27
+        #define REG1_APP_PIN4 26
+        #define REG1_APP_PIN5 18
+        #define REG1_APP_PIN6 17
+        #define REG1_APP_PIN7 16
     #endif
 
 // REG1-IpController2040
@@ -141,6 +173,115 @@
         #define PIN_ETH_SS (1)
         #define PIN_ETH_INT (5)
         #define PIN_ETH_RES (4)
+    #endif
+
+// REG1-ControllerESP
+// https://github.com/OpenKNX/OpenKNX/wiki/REG1-ControllerESP
+     #ifdef OKNXHW_REG1_CONTROLLERESP
+        #ifndef HARDWARE_NAME
+            #define HARDWARE_NAME "OpenKNX-REG1-ControllerESP"
+        #endif
+
+        #ifdef USE_RGBLED
+            #define RGBLED_PIN 4
+            #define RGBLED_NUM 4
+            #define PROG_LED_PIN 0
+            #define INFO1_LED_PIN 1
+            #define INFO2_LED_PIN 2
+            #define INFO3_LED_PIN 3
+        #else
+            #define PROG_LED_PIN 4
+            #define PROG_LED_PIN_ACTIVE_ON HIGH
+        #endif
+
+
+        #define PROG_BUTTON_PIN 39
+        #define PROG_BUTTON_PIN_INTERRUPT_ON FALLING
+
+        #define SAVE_INTERRUPT_PIN 36
+        #define KNX_UART_RX_PIN 37
+        #define KNX_UART_TX_PIN 14
+
+        #define FUNC1_BUTTON_PIN 38
+        #define FUNC2_BUTTON_PIN 34
+        #define FUNC3_BUTTON_PIN 35
+
+        #define ETH_PHY_TYPE  ETH_PHY_LAN8720   // type of PHY used, needed for IDF
+        #define ETH_PHY_ADDR  0                 // PHYs I2C address
+        #define ETH_PHY_MDC   33                
+        #define ETH_PHY_MDIO  32
+        #define ETH_PHY_POWER 2                 // enable / disable the PHYs clock
+        #define ETH_CLK_MODE  ETH_CLOCK_GPIO0_IN
+
+        #define REG1_APP_PIN1 12
+        #define REG1_APP_PIN2 15
+        #define REG1_APP_PIN3 13
+        #define REG1_APP_PIN4 5
+        #define REG1_APP_PIN5 8
+        #define REG1_APP_PIN6 7
+        #define REG1_APP_PIN7 20
+
+        // informative pin usage - Do not use for other purposes
+        // RMII: 
+        // ETH_PHY_CLK_INPUT_PIN 0
+        // ETH_PHY_RXD0_PIN 25
+        // ETH_PHY_RXD1_PIN 26
+        // ETH_PHY_RX_DV_CRS_PIN 27
+        // ETH_PHY_TX_EN_PIN 21
+        // ETH_PHY_TXD0_PIN 19
+        // ETH_PHY_TXD1_PIN 22
+        //
+        // USB:
+        // USB_UART_TX_PIN 1
+        // USB_UART_RX_PIN 3
+    #endif
+
+// REG1-App-SEN-Multi
+// https://github.com/OpenKNX/OpenKNX/wiki/REG1-App-SEN-Multi
+    #ifdef OKNXHW_REG1_APP_SEN_MULTI
+        #define SENSOR1_SDA_TX_PIN  REG1_APP_PIN4
+        #define SENSOR1_SCL_RX_PIN  REG1_APP_PIN3
+        #define SENSOR2_SDA_TX_PIN  REG1_APP_PIN2
+        #define SENSOR2_SCL_RX_PIN  REG1_APP_PIN1
+        #define BINARY_INPUT1_PIN   REG1_APP_PIN7
+        #define BINARY_INPUT2_PIN   REG1_APP_PIN6
+        #define BINARY_INPUT3_PIN   REG1_APP_PIN5
+    #endif
+
+// REG1-App-GW-RF2G4
+// https://github.com/OpenKNX/OpenKNX/wiki/REG1-App-GW-RF2G4
+    #ifdef OKNXHW_REG1_APP_GW_RF2G4
+        #define RF_CS       REG1_APP_PIN1
+        #define RF_MISO     REG1_APP_PIN2
+        #define RF_MOSI     REG1_APP_PIN3
+        #define RF_CLK      REG1_APP_PIN4
+        #define RF_INT      REG1_APP_PIN5
+        #define RF_ENABLE   REG1_APP_PIN6
+    #endif
+
+// REG1-App-SA-4xSELV
+// https://github.com/OpenKNX/OpenKNX/wiki/REG1-App-SA-4xSELV
+    #ifdef OKNXHW_REG1_APP_SA_4xSELV
+        #define OPENKNX_SWA_CHANNEL_COUNT 4
+        #define OPENKNX_SWA_SET_PINS 0x0106, 0x0104, 0x0100, 0x0102
+        #define OPENKNX_SWA_RESET_PINS 0x0107, 0x0105, 0x0101, 0x0103
+        #define OPENKNX_SWA_SET_ACTIVE_ON HIGH
+        #define OPENKNX_SWA_RESET_ACTIVE_ON HIGH
+        #define OPENKNX_SWA_BISTABLE_IMPULSE_LENGTH 50
+
+        #define OPENKNX_GPIO_NUM 1
+        #define OPENKNX_GPIO_TYPES OPENKNX_GPIO_T_TCA6408
+        #define OPENKNX_GPIO_ADDRS 0x20
+        #define OPENKNX_GPIO_INTS 0xFF
+        #define OPENKNX_GPIO_RESETS REG1_APP_PIN4
+
+        #define OPENKNX_GPIO_WIRE Wire
+        #define OPENKNX_GPIO_CLOCK 400000
+        #define OPENKNX_GPIO_SDA REG1_APP_PIN2
+        #define OPENKNX_GPIO_SCL REG1_APP_PIN1
+
+        #define V12_ADC_PIN REG1_APP_PIN3
+
     #endif
 
 // UP1-Controller2040
@@ -235,5 +376,3 @@
         #define KNX_UART_RX_PIN 6
         #define KNX_UART_TX_PIN 7
     #endif
-
-#endif
