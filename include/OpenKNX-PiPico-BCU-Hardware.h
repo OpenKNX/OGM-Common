1860096bfa1558520649df1b523c499a77ddd235
Erkan Çolak <erkanc@gmx.de>
2024-11-11 17:54:10 +0100

    OpenKNXHardware definition - OpenKNX-PiPico-BCU-Hardware.h

---
+++ include/OpenKNX-PiPico-BCU-Hardware.h
@@ -0,0 +1,30 @@
+#pragma once
+/**
+ * OpenKNX Hardware definition header file
+ * 
+ * File: OpenKNX-PiPico-BCU-Hardware.h
+ * Hardware: OpenKNX PiPico BCU Connector
+ * Responsible: OpenKNX
+ *
+ * Defines hardware IO pins and functionalities for the OpenKNX PiPico BCU Connector.
+ * Includes pin assignments for LEDs, buttons, serial communication, and other peripherals.
+ * Ensures compatibility with various application boards and firmware features.
+ *
+ * More info about the Hardware visit: https://github.com/OpenKNX/OpenKNX/wiki/PiPico-BCU-Connector
+ *
+ * ATTENTION:
+ *    Do not include this file directly.
+ *    It will be included by the OpenKNXHardware.h file.
+ */
+
+// PiPico-BCU-Connector
+#ifdef OKNXHW_PIPICO_BCU_CONNECTOR
+    #define PROG_LED_PIN 21
+    #define PROG_LED_PIN_ACTIVE_ON HIGH
+    #define PROG_BUTTON_PIN 22
+    #define PROG_BUTTON_PIN_INTERRUPT_ON FALLING
+    #define SAVE_INTERRUPT_PIN 20
+    #define KNX_SERIAL Serial1
+    #define KNX_UART_RX_PIN 1
+    #define KNX_UART_TX_PIN 0
+#endif
