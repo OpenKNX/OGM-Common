60dd09c252127f7107fa7a941231cdd1d8547677
Mike <45664417+thewhobox@users.noreply.github.com>
2025-01-07 16:49:24 +0100

    fix: prepare script

--- scripts/pio/prepare.py
+++ scripts/pio/prepare.py
@@ -139,11 +139,9 @@ for name, version in openknx_modules.items():
   define_name = "MODULE_" + name.split("-")[1]
   version_file.write("#define {} \"{}\"\n".format(define_name + "_Version", version))
   versions = version.split("+")[0].split(".")
-  if len(versions) > 0:
+  if len(versions) == 3:
     version_file.write("#define {}_Version_Major {}\n".format(define_name, int(versions[0])))
-  if len(versions) > 1:
     version_file.write("#define {}_Version_Minor {}\n".format(define_name, int(versions[1])))
-  if len(versions) > 2:
     version_file.write("#define {}_Version_Revision {}\n".format(define_name, int(versions[2])))
 
   ets = get_ets_version(version)
