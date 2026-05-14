075632a3204d04094933ecbcc0c26d02cf37196a
Michael Geramb <lasso_riefen0m@icloud.com>
2026-05-14 12:59:39 +0200

    Make max brightness of GPIO LED configurable

--- src/OpenKNX/Led/GPIO.h
+++ src/OpenKNX/Led/GPIO.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "OpenKNX/Led/Base.h"
 
+#ifndef OPENKNX_LEDGPIO_MAX_BRIGHTNESS
+    #define OPENKNX_LEDGPIO_MAX_BRIGHTNESS 255
+#endif
+
 namespace OpenKNX
 {
     namespace Led
@@ -15,7 +19,7 @@ namespace OpenKNX
             void writeLed(uint8_t brightness) override;
 
           public:
-            GPIO(long pin = -1, long activeOn = HIGH, bool isDimmable = true);
+            GPIO(long pin = -1, long activeOn = HIGH, bool isDimmable = true, uint8_t maxBrightness = OPENKNX_LEDGPIO_MAX_BRIGHTNESS);
             void init() override;
 
             long getPin() { return _pin; }
